67a9f83592ff4fcb3dd8dfc1549801b036690278
rizrmdhn <rizrmdhn.work@gmail.com>
2023-11-17 09:19:33 +0800

    update

---
+++ client/src/types/index.d.ts
@@ -0,0 +1,298 @@
+export type TError = {
+  rule: string;
+  field: string;
+  message: string;
+};
+
+export type TErrorResponse = {
+  meta: {
+    status: number;
+    message: string;
+  };
+  errors: TError[];
+};
+
+export type TMeta = {
+  status: number;
+  message: string;
+};
+
+export type TNote = {
+  id: number;
+  title: string;
+  content: string;
+  slug: string;
+  tags: string[];
+  owner_id: number;
+  folder_id: number;
+  is_friend_only: boolean;
+  is_public: boolean;
+  is_private: boolean;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+  is_deleted: boolean;
+};
+
+export type TToken = {
+  type: string;
+  token: string;
+};
+
+export type TLoginResponse = {
+  meta: TMeta;
+  data: { token: TToken; user: TUser };
+};
+
+export type TRegisterResponse = {
+  meta: TMeta;
+  data: {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+    avatar: string;
+    created_at: string;
+    updated_at: string;
+  };
+};
+
+export type TGetNoteResponse = {
+  meta: TMeta;
+  data: TNote[];
+};
+
+export type TCreateNote = {
+  id: number;
+  title: string;
+  content: string;
+  slug: string;
+  tags: string[];
+  owner_id: number;
+  folder_id: number;
+  is_friend_only: boolean;
+  is_public: boolean;
+  is_private: boolean;
+  created_at: string;
+  updated_at: string;
+};
+
+export type TCreateNoteResponse = {
+  meta: TMeta;
+  data: {
+    id: number;
+    title: string;
+    content: string;
+    slug: string;
+    tags: string[];
+    owner_id: number;
+    folder_id: number;
+    is_friend_only: boolean;
+    is_public: boolean;
+    is_private: boolean;
+    created_at: string;
+    updated_at: string;
+  };
+};
+
+export type TUpdateNoteResponse = {
+  meta: TMeta;
+  data: TNote;
+};
+
+export type TDeleteNoteResponse = {
+  meta: TMeta;
+};
+
+export type TRestoreDeletedNoteResponse = {
+  meta: TMeta;
+};
+
+export type TGetNoteByIdResponse = {
+  meta: TMeta;
+  data: TNote;
+};
+
+export type TGetNoteBySlugResponse = {
+  meta: TMeta;
+  data: TNote;
+};
+
+export type TCategories = {
+  id: number;
+  name: string;
+  owner_id: number;
+  is_public: boolean;
+  is_private: boolean;
+  is_friend_only: boolean;
+  is_deleted: boolean;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+};
+
+export type TGetCategoriesResponse = {
+  meta: TMeta;
+  data: TCategories[];
+};
+
+export type TCreateCategoriesResponse = {
+  meta: TMeta;
+  data: TCategories;
+};
+
+export type TGetCategoryByIdResponse = {
+  meta: TMeta;
+  data: TCategories;
+};
+
+export type TUpdateCategoriesResponse = {
+  meta: TMeta;
+  data: TCategories;
+};
+
+export type TDeleteCategoriesResponse = {
+  meta: TMeta;
+};
+
+export type TGetDeletedCategoriesResponse = {
+  meta: TMeta;
+  data: TCategories[];
+};
+
+export type TRestoreDeletedCategoriesResponse = {
+  meta: TMeta;
+};
+
+export type TBulkDeleteCategoriesResponse = {
+  meta: TMeta;
+};
+
+export type TPermanentDeleteCategoriesResponse = {
+  meta: TMeta;
+};
+
+export type TFolder = {
+  id: number;
+  name: string;
+  owner_id: number;
+  is_public: boolean;
+  is_private: boolean;
+  is_friend_only: boolean;
+  is_deleted: boolean;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+};
+
+export type TGetFoldersResponse = {
+  meta: TMeta;
+  data: TFolder[];
+};
+
+export type TGetDeletedFoldersResponse = {
+  meta: TMeta;
+  data: TFolder[];
+};
+
+export type TCreateFolderResponse = {
+  meta: TMeta;
+  data: TFolder;
+};
+
+export type TGetFolderByIdResponse = {
+  meta: TMeta;
+  data: TFolder;
+};
+
+export type TUpdateFolderResponse = {
+  meta: TMeta;
+  data: TFolder;
+};
+
+export type TDeleteFolderResponse = {
+  meta: TMeta;
+};
+
+export type TRestoreDeletedFolderResponse = {
+  meta: TMeta;
+};
+
+export type TFriendList = {
+  friend_id: number;
+  friend_username: string;
+  friend_email: string;
+};
+
+export type TFriendRequestOrSent = {
+  id: number;
+  username: string;
+  email: string;
+};
+
+export type TGetFriendResponse = {
+  meta: TMeta;
+  data: {
+    friendList: TFriendList[];
+    friendRequestList: TFriendRequestOrSent[];
+    friendSentList: TFriendRequestOrSent[];
+  };
+};
+
+export type TSentFriendRequestResponse = {
+  meta: TMeta;
+  data: {
+    id: number;
+    sender_id: number;
+    receiver_id: number;
+    created_at: string;
+  };
+};
+
+export type TAcceptFriendRequestResponse = {
+  meta: TMeta;
+  data: {
+    id: number;
+    user_id: number;
+    friend_id: number;
+    created_at: string;
+  };
+};
+
+export type TRejectFriendRequestResponse = {
+  meta: TMeta;
+};
+
+export type TCancleFriendRequestResponse = {
+  meta: TMeta;
+};
+
+export type TDeleteFriendResponse = {
+  meta: TMeta;
+};
+
+export type TUser = {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+  avatar: string;
+  remember_me_token?: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type TGetMeResponse = {
+  meta: TMeta;
+  data: TUser;
+};
+
+export type TGetUserListResponse = {
+  meta: TMeta;
+  data: TUser[];
+};
+
+export type TUpdateAvatarResponse = {
+  meta: TMeta;
+  data: TUser;
+};
---
+++ client/src/utils/api.ts
@@ -0,0 +1,267 @@
+import {
+  TCreateNote,
+  TCreateNoteResponse,
+  TDeleteNoteResponse,
+  TGetNoteByIdResponse,
+  TGetNoteResponse,
+  TLoginResponse,
+  TNote,
+  TRegisterResponse,
+  TRestoreDeletedNoteResponse,
+  TUpdateNoteResponse,
+  TUser,
+} from "@/types";
+import axios from "axios";
+
+const baseUrl = "http://127.0.0.1:3333";
+
+const localStorageKey = "__notes_app_token__";
+
+const localStorageFunc = (() => {
+  function setToken(token: string): void {
+    return localStorage.setItem("accessToken", token);
+  }
+
+  function getToken(): string | null {
+    return localStorage.getItem(localStorageKey);
+  }
+
+  function removeToken() {
+    return localStorage.removeItem(localStorageKey);
+  }
+
+  return {
+    setToken,
+    getToken,
+    removeToken,
+  };
+})();
+
+const auth = (() => {
+  async function login(email: string, password: string): Promise<TUser> {
+    const response = await axios.post(`${baseUrl}/login`, {
+      email,
+      password,
+    });
+
+    const { meta } = response.data as TLoginResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const {
+      data: { token, user },
+    } = response.data as TLoginResponse;
+
+    localStorageFunc.setToken(token.token);
+
+    return user;
+  }
+
+  async function register(
+    name: string,
+    email: string,
+    username: string,
+    password: string,
+  ): Promise<TUser> {
+    const response = await axios.post(`${baseUrl}/register`, {
+      name,
+      email,
+      username,
+      password,
+    });
+
+    const { meta } = response.data as TRegisterResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TRegisterResponse;
+
+    return data;
+  }
+
+  return {
+    login,
+    register,
+  };
+})();
+
+const notes = (() => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async function _fetchWithAuth(url: string, options: any) {
+    return axios(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${localStorageFunc.getToken()}`,
+      },
+    });
+  }
+
+  async function getNotes(): Promise<TNote[]> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes`, {
+      method: "GET",
+    });
+
+    const { meta } = response.data as TGetNoteResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TGetNoteResponse;
+
+    return data;
+  }
+
+  async function getDeletedNotes(): Promise<TNote[]> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes/deleted`, {
+      method: "GET",
+    });
+
+    const { meta } = response.data as TGetNoteResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TGetNoteResponse;
+
+    return data;
+  }
+
+  async function createNote(
+    title: string,
+    content: string,
+    tags: string[],
+  ): Promise<TCreateNote> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes`, {
+      method: "POST",
+      data: {
+        title,
+        content,
+        tags,
+      },
+    });
+
+    const { meta } = response.data as TCreateNoteResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TCreateNoteResponse;
+
+    return data;
+  }
+
+  async function updateNote(
+    id: number,
+    title: string,
+    content: string,
+    tags: string[],
+    isPublic: boolean,
+    isPrivate: boolean,
+    isFriendOnly: boolean,
+  ): Promise<TNote> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes/${id}`, {
+      method: "PUT",
+      data: {
+        title,
+        content,
+        tags,
+        is_public: isPublic,
+        is_private: isPrivate,
+        is_friend_only: isFriendOnly,
+      },
+    });
+
+    const { meta } = response.data as TUpdateNoteResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TUpdateNoteResponse;
+
+    return data;
+  }
+
+  async function deleteNote(id: number): Promise<TDeleteNoteResponse> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes/${id}`, {
+      method: "DELETE",
+    });
+
+    const { meta } = response.data as TDeleteNoteResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    return response.data as TDeleteNoteResponse;
+  }
+
+  async function restoreDeletedNote(
+    id: number,
+  ): Promise<TRestoreDeletedNoteResponse> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes/${id}/restore`, {
+      method: "PUT",
+    });
+
+    const { meta } = response.data as TRestoreDeletedNoteResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    return response.data as TRestoreDeletedNoteResponse;
+  }
+
+  async function getNoteById(id: number): Promise<TNote> {
+    const response = await _fetchWithAuth(`${baseUrl}/notes?id=${id}`, {
+      method: "GET",
+    });
+
+    const { meta } = response.data as TGetNoteByIdResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TGetNoteByIdResponse;
+
+    return data;
+  }
+
+  async function getNoteBySlug(slug: string): Promise<TNote> {
+    const response = await _fetchWithAuth(`${slug}`, {
+      method: "GET",
+    });
+
+    const { meta } = response.data as TGetNoteByIdResponse;
+
+    if (meta.message !== "Success") {
+      throw new Error(meta.message);
+    }
+
+    const { data } = response.data as TGetNoteByIdResponse;
+
+    return data;
+  }
+
+  return {
+    getNotes,
+    getDeletedNotes,
+    createNote,
+    updateNote,
+    deleteNote,
+    restoreDeletedNote,
+    getNoteById,
+    getNoteBySlug,
+  };
+})();
+
+export { auth, notes };
